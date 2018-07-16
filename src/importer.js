442dfbcf26236f677fd253d979c9f57e0070410f
David Lam <dav_lam@ymail.com>
2018-07-16 07:07:11 -0700

    added importer

---
+++ src/importer.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
+const path = require("path");
+const jsonfile = require("jsonfile");
+
+const url = process.env.MONGO_URI;
+
+const currentVersion = "v20";
+const data = jsonfile.readFileSync(path.join(__dirname, "..", "data", `servants_${currentVersion}.clean.json`));
+
+MongoClient.connect(url, {useNewUrlParser: true}, (error, client) => {
+  if (error) {
+    console.error(`failed to connect ${error.message}`);
+    return;
+  }
+  console.log("connection success");
+
+  const db = client.db("fatego-db");
+  const servants = db.collection("servants");
+
+  data.forEach((datum) => {
+    servants.findOneAndUpdate(datum, {$setOnInsert: datum}, {upsert: true}, (error, result) => {
+      if (error) {
+        console.error(`failed to add ${datum.name} : ${error.message}`);
+      } else {
+        console.log(`insert success ${datum.name}`);
+      }
+    });
+  });
+
+  client.close();
+});
